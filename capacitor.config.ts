cf73848a1d2b61ba23ef62c91e84dff614d5c618
Felix Olszewski <e12022502@student.tuwien.ac.at>
2024-08-29 08:37:58 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'firebase-observable-double-emission',
+  webDir: 'www'
+};
+
+export default config;
